101a05bd0eee4db9c16846fb3a83366d3324a848
ssactl_jianishe <1285092865@qq.com>
2023-09-05 20:11:27 +0200

    lab2  1.1 Create a simple map with Leaflet

--- lab2/app.js
+++ lab2/app.js
@@ -1,8 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.use(express.static('public'))
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
 
-app.get('/', (req, res) => res.send('This is web and mobile gis course, lab2!'))
-app.get('/lab2part1', (req, res) => res.sendFile(__dirname + '/lab2part1.html'))
-app.listen(port, () => console.log(`Example app listening on port ${ port }!`))
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(
+  "/scripts",
+  express.static(path.join(__dirname + "/node_modules/leaflet/dist/"))
+);
+
+app.get("/", (req, res) =>
+  res.send("This is web and mobile gis course, lab2!")
+);
+app.get("/lab2part1", (req, res) =>
+  res.sendFile(__dirname + "/lab2part1.html")
+);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
